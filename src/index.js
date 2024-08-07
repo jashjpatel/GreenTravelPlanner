9402ff0c8955428ac294dcf13ec6fa706eabe2fd
Jash Patel <jashpatelj@gmail.com>
2024-08-07 10:54:23 -0400

    setting up the express server

--- src/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('myApp', [])
-        .controller('MainController', ['$scope', function($scope) {
-            $scope.title = 'Hello, AngularJS!';
-        }]);
-})();
